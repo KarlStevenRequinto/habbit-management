bf69bb42d6bc8655fc685affd4709c13b16700ad
KarlStevenRequinto <lerdrobott@gmail.com>
2025-08-05 23:40:25 +0800

    fix layout expenses

--- src/app/(main)/expenses/page.tsx
+++ src/app/(main)/expenses/page.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const ExpensesPage = () => {
     return (
-        <div className="h-full w-full flex items-center justify-center bg-red-100">
-            <div className="h-fit w-fit bg-red-300 p-4 rounded">ExpensesPage</div>
+        <div className="h-full w-full flex flex-col bg-red-100">
+            {/* Top section: 2/3 height */}
+            <div className="flex-2 flex flex-col lg:flex-row w-full">
+                <div className="flex-1 bg-blue-200">Box 1</div>
+                <div className="flex-1 bg-green-200">Box 2</div>
+            </div>
+
+            {/* Bottom section: 1/3 height */}
+            <div className="flex-1 bg-yellow-200">Big Box Below</div>
         </div>
     );
 };
